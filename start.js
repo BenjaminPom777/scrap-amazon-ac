d5dd44c0c7d4cbcfad20119afb41c48720806479
beni <mamont4444@live.ru>
2024-02-28 11:30:44 +0200

    muliple items, also our items first

--- start.js
+++ start.js
@@ -1,31 +1,51 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const ExcelJS = require('exceljs');
+const mostSellingItems = require('./most-selling.json');
 
-const scrapeAutocomplete = async (searchTerm) => {
+const scrapeAutocomplete = async (searchTerms) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto('https://amazon.com');
-
+  await new Promise(resolve => setTimeout(resolve, 500));
   const searchSelector = '#twotabsearchtextbox';
-  await page.waitForSelector(searchSelector);
-  await page.type(searchSelector, searchTerm);
+  await page.waitForSelector(searchSelector);   //error: Waiting for selector `#twotabsearchtextbox` failed: Waiting failed: 30000ms exceeded   need to fix
+  let allAutocompleteList = [];
 
-  // Wait for the autocomplete suggestions to appear
-  const autocompleteSelector = '.s-suggestion-container';
-  await page.waitForSelector(autocompleteSelector);
-
-
-  const autocompleteList = await page.evaluate(async (selector) => {
-    await new Promise(resolve => setTimeout(resolve, 2000)); //delay for results to be more accurate 
-    const suggestions = document.querySelectorAll(selector);
-    return Array.from(suggestions, suggestion => suggestion.innerText);
-  }, autocompleteSelector);
+  let appear = false;
+  for (const searchTerm of searchTerms) {
 
 
-  fs.writeFileSync('autocomplete_data.json', JSON.stringify(autocompleteList, null, 2));
+    await page.click(searchSelector, { clickCount: 3 }) //trick to clean input
+    await page.type(searchSelector, searchTerm);
 
+    // Wait for the autocomplete suggestions to appear
+    const autocompleteSelector = '.s-suggestion-container';
+    if (!appear) {
+      await page.waitForSelector(autocompleteSelector);
+      appear = true;
+    }
+
+
+    const autocompleteList = await page.evaluate(async (selector) => {
+      await new Promise(resolve => setTimeout(resolve, 2000)); //delay for results to be more accurate 
+      const suggestions = document.querySelectorAll(selector);
+      return Array.from(suggestions, suggestion => suggestion.innerText);
+    }, autocompleteSelector);
+
+    allAutocompleteList = [...allAutocompleteList, ...autocompleteList];
+  }
+
+  await browser.close();
+
+  return allAutocompleteList;
+}
+
+
+const printToXml = async (items) => {
+
+  fs.writeFileSync('autocomplete_data.json', JSON.stringify(items, null, 2));
 
   const workbook = new ExcelJS.Workbook();
   const worksheet = workbook.addWorksheet('Autocomplete Data');
@@ -34,13 +54,33 @@ const scrapeAutocomplete = async (searchTerm) => {
     { header: 'Autocomplete Suggestions', key: 'suggestion' }
   ];
 
-  autocompleteList.forEach(suggestion => {
+  items.forEach(suggestion => {
     worksheet.addRow({ suggestion });
   });
 
   await workbook.xlsx.writeFile('autocomplete_data.xlsx');
-
-  await browser.close();
 }
 
-scrapeAutocomplete('lol');
+
+const flow = async (userInput) => {
+
+  const itemsToScrap = [];
+
+  for (const msItem of mostSellingItems) {
+    if (msItem.toLowerCase().startsWith(userInput)) {
+      itemsToScrap.push(msItem.toLowerCase());
+    }
+  }
+  if (!itemsToScrap.includes(userInput)) { // also insure that this item is last
+    itemsToScrap.push(userInput);
+  }
+
+  const searchTermsToPrint = await scrapeAutocomplete(itemsToScrap);
+
+  if (searchTermsToPrint.length > 0) {
+    printToXml(searchTermsToPrint);
+  }
+
+}
+
+flow('mo')
